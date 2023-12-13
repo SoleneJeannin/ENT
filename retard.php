07657a1c22cd8ace14e046302cd997fdf39f24cb
Andrea32004 <andrea.laizeau@gmail.com>
2023-12-13 10:50:48 +0100

    retard

---
+++ retard.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;200;300;400;500;600;700;800;900&family=Lusitana:wght@400;700&display=swap"
+        rel="stylesheet">
+        <link rel="stylesheet" href="style_de_base.css">
+    <title>Document, retard, absence</title>
+
+    
+</head>
+
+<body>
+
+    <main>
+    
+
+        <?php
+
+        include('nav.php');
+        
+        ?>
+        
+
+    </main>
+
+
+</body>
+
+</html>
