5a431954a36dee09ab967fc33a27f633a8741a75
SoleneJeannin <solejea77@gmail.com>
2023-12-14 15:28:40 +0100

    Modif  reservation salle + materiel
    début gestion de stockage materiel + début affichage + reserver salles

--- reservation_salle.php
+++ reservation_salle.php
@@ -21,11 +21,9 @@
     
 
         <?php
-
+        session_start();
         include('nav.php');
         include('connexion.php');
-        session_start();
-        
         ?>
         <h1>Réservez votre salle</h1>
 
@@ -35,11 +33,38 @@
 
         <div>
             <?php
-            $stmt_cam=$db->query("SELECT * FROM salle");
+            $stmt=$db->query("SELECT * FROM salle");
             $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
-            foreach($result as $row){ ?>
-                <!--Nom de la salle-->
-                <h3><?= $row['salle_nom']?></h3>               
+            foreach($result as $row){ 
+            //Affiche toutes les salles + quand on clique sur une des salles ça envoie son numéro en GET
+            ?>
+                <a href="reservation_salle?id=<?= $row['id_salle']?>">
+                    <!--Numéro de la salle-->
+                    <?= $row['id_salle']?>
+                </a>             
+            <?php
+            }
+            //Si on a cliqué sur une salle : 
+            if(isset($_GET['id'])){ 
+                $num_salle = $_GET['id'];
+                ?>
+                <div>
+                    <h2>Salle <?= $num_salle ?></h2> <?php 
+                     $stmt_salle=$db->query("SELECT * FROM salle WHERE id_salle = $num_salle");
+                    $result_salle=$stmt_salle->fetch(PDO::FETCH_ASSOC);
+                    //Est ce que la salle est occupée ?
+                    if($result_salle['statut'] == '1'){ // Si oui : 
+                        echo 'Salle non disponible';
+                    } else{ // Si non : ?>
+                        <p>Salle disponible de ? à ?</p>
+                        <!--Faire juste 4 crénaux possibles (8-10 10-12 14-16 16-18)-->
+                        <form action="">
+                            <input type="submit" value="Réserver">
+                        </form>
+                    <?php
+                    }
+                    ?>                    
+                </div>
             <?php
             }
             ?>
