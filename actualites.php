c108d4e07348ab875de107e7eb5ea0043ba47be7
dainay <115573080+dainay@users.noreply.github.com>
2023-12-14 16:45:02 +0100

    FIXED NAV

--- actualites.php
+++ actualites.php
@@ -11,7 +11,7 @@ session_start();
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;200;300;400;500;600;700;800;900&family=Lusitana:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style_de base.css">
+    <link rel="stylesheet" href="style_de_base.css">
     <link rel="stylesheet" href="css/actualites.css">
     <title>Actualités</title>
 </head>
--- evals.php
+++ evals.php
@@ -12,7 +12,7 @@ session_start();
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;200;300;400;500;600;700;800;900&family=Lusitana:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style_de base.css">
+    <link rel="stylesheet" href="style_de_base.css">
     <title>Évaluations</title>
 
 
