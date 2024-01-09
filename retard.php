fdc2bdc2896949d29f2ff0579548116e0d9e8001
SoleneJeannin <solejea77@gmail.com>
2024-01-09 13:31:24 +0100

    retard fini
    resolution de conflit

--- retard.php
+++ retard.php
@@ -26,7 +26,6 @@
         include('nav.php');
         include('connexion.php');
 
-<<<<<<< HEAD
         if(isset($_SESSION['login'])){
             $user = $_SESSION['login'];
 
@@ -130,74 +129,6 @@
             <?php
             }
     ?>
-=======
-        //Récupération de l'utilisateur
-        $stmt = $db->query("SELECT * FROM user WHERE user_login = '" . $_SESSION['login'] . "'");
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        $_SESSION['id_user'] = $result['id_user'];
-        ?>
-
-
-        <h1>Mes documents</h1>
-        <div class="bloc_document">
-            <div class="first_colonne">
-                <a href="./document/étudiants/<?= $_SESSION['id_user'] ?>/certificat_sco.pdf"
-                    class="lien_document">Certificat de scolarité</a>
-                <a href="./document/étudiants/<?= $_SESSION['id_user'] ?>/bourse.pdf" class="lien_document">Notification
-                    conditionnelle de bourse</a>
-                <a href="./document/étudiants/<?= $_SESSION['id_user'] ?>carte_etu.pdf" class="lien_document">Carte
-                    étudiant éléctronique</a>
-            </div>
-
-            <div class="ligne"></div>
-            <div class="second_colonne">
-                <a href="./document/étudiants/<?= $_SESSION['id_user'] ?>/stage1.pdf" class="lien_document">Contrat de
-                    stage (2e semestre)</a>
-                <a href="./document/étudiants/<?= $_SESSION['id_user'] ?>/stage2.pdf" class="lien_document">Contrat de
-                    stage (4e semestre)</a>
-                <a href="./document/étudiants/<?= $_SESSION['id_user'] ?>/alternance.pdf" class="lien_document">Contrat
-                    d'alternance</a>
-            </div>
-        </div>
-        <br>
-        <div>
-            <p>Retards :
-                <?= $result['user_retard'] ?> minutes
-            </p>
-
-            <?php
-            //Récupérer les absences de l'utilisateur
-            $id_user = $_SESSION['id_user'];
-
-            $stmt2 = $db->query("SELECT * FROM absence WHERE ext_etudiant = '" . $id_user . "'");
-            $result2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-            $abs_justif = 0;
-            $abs_non_justif = 0;
-            foreach ($result2 as $row) {
-                //Si le champ "jusitifcation" n'est pas vide (que l'absence est justifiée)
-                if (isset($row['abs_justificatif'])) {
-                    //Additionner toutes les absences
-                    $abs_justif = $abs_justif + $row['abs_duree'];
-                }
-                //Si le champ "jusitifcation" vaut "NULL" (que l'absence est NON justifiée)
-                else {
-                    $abs_non_justif = $abs_non_justif + $row['abs_duree'];
-                }
-            }
-
-            ?>
-            <p>Absences :
-                <?= $abs_justif ?> heure(s) justifiée(s) et
-                <?= $abs_non_justif ?> heure(s) non justifiée(s)
-            </p>
-
-        </div>
-
-        <br><br>
-
-        <p class="info">Si vous avez des questions sur vos démarches administratives, vous pouvez vous adresser à votre
-            responsable pédagogique : Sophie DAVID</p>
->>>>>>> 8ad53df24b6cbfa12dedaad99fcbbac463b55657
     </main>
 
 
